c4391467c040fe243052216391b560191557ae9d
Kim001104 <dongsky2000@naver.com>
2026-01-29 14:19:56 +0900

    ADC

---
+++ Ex_ADC/TriCore Debug (TASKING)/ADC_Background_Ex.d
@@ -0,0 +1,116 @@
+ADC_Background_Ex.o :	../ADC_Background_Ex.c
+../ADC_Background_Ex.c :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\\Vadc\\Adc\IfxVadc_Adc.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\\Vadc\\Adc\IfxVadc_Adc.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Vadc\Std\IfxVadc.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Vadc\Std\IfxVadc.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxVadc_cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxVadc_cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Src\Std\IfxSrc.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Src\Std\IfxSrc.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxSrc_cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxSrc_cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Configurations\Ifx_Cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Configurations\Ifx_Cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+ADC_Background_Ex.o :	"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxSrc_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\Ifx_TypesReg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\Ifx_TypesReg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxVadc_PinMap.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxVadc_PinMap.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxPort_cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxPort_cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxPort_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxScu_cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxScu_cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_bf.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_bf.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_bf.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_bf.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxScu_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.asm.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.asm.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\Ifx_PinMap.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\Ifx_PinMap.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_bf.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxVadc_bf.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxCpu_cfg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_Impl\IfxCpu_cfg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCpu_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxStm_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuCcu.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxFlash_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxScu_PinMap.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\_PinMap\IfxScu_PinMap.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Port\Std\IfxPort.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\iLLD\\TC27D\\Tricore\Scu\Std\IfxScuWdt.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_reg.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_reg.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_regdef.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_regdef.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_bf.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Infra\\Sfr\\TC27D\\_Reg\IfxCcu6_bf.h" :
+ADC_Background_Ex.o :	"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"C:\\aurix_workspace\\Ex_ADC\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+ADC_Background_Ex.o :	..\ADC_Background_Ex.h
+..\ADC_Background_Ex.h :
