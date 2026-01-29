17920bd415e3babe58e3dce664bfde831d36272e
Kim001104 <dongsky2000@naver.com>
2026-01-29 16:07:40 +0900

    PWM1_testcode

---
+++ GTM_TOM_PWM_Ex/TriCore Debug (TASKING)/Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.d
@@ -0,0 +1,18 @@
+IfxCif_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.c
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxCif_cfg.c :
+IfxCif_cfg.o :	..\Libraries\iLLD\TC27D\Tricore\_Impl\IfxCif_cfg.h
+..\Libraries\iLLD\TC27D\Tricore\_Impl\IfxCif_cfg.h :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Configurations\Ifx_Cfg.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Configurations\Ifx_Cfg.h" :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+IfxCif_cfg.o :	"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h" :
+IfxCif_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
---
+++ GTM_TOM_PWM_Ex/TriCore Debug (TASKING)/Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.d
@@ -0,0 +1,22 @@
+IfxFlash_cfg.o :	../Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.c
+../Libraries/iLLD/TC27D/Tricore/_Impl/IfxFlash_cfg.c :
+IfxFlash_cfg.o :	..\Libraries\iLLD\TC27D\Tricore\_Impl\IfxFlash_cfg.h
+..\Libraries\iLLD\TC27D\Tricore\_Impl\IfxFlash_cfg.h :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_Intrinsics.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Configurations\Ifx_Cfg.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Configurations\Ifx_Cfg.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+IfxFlash_cfg.o :	"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.28\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
+IfxFlash_cfg.o :	"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h"
+"C:\\aurix_workspace\\GTM_TOM_PWM_Ex\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\IfxCpu_IntrinsicsTasking.h" :
